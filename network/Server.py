73ed730f774f94fa30fba8e275abe869919ab4a9
tompinn23 <tompinncock@gmail.com>
2017-01-24 21:45:40 +0000

    more mp

--- network/Server.py
+++ network/Server.py
@@ -9,7 +9,6 @@ import time
 
 logging.basicConfig(filename='server.log',level=logging.INFO)
 
-_map = Map(70, 50)
 from weakref import WeakKeyDictionary
 
 from network.PodSixNet.Server import Server
@@ -18,8 +17,7 @@ from network.PodSixNet.Channel import Channel
 class ClientChannel(Channel):
     """
     	This is the server representation of a single connected client.
-    	"""
-
+    """
     def __init__(self, *args, **kwargs):
         self.nickname = "anonymous"
         Channel.__init__(self, *args, **kwargs)
@@ -31,15 +29,15 @@ class ClientChannel(Channel):
     ### Network specific callbacks ###
     ##################################
 
-    def Network_message(self, data):
-        self._server.SendToAll({"action": "message", "message": data['message'], "who": self.nickname})
+    def Network(self, data):
+        self.Send({"action": "message", "message": data["message"]})
 
     def Network_nickname(self, data):
         self.nickname = data['nickname']
-        self._server.SendPlayers()
 
     def Network_connected(self,data):
         print("connectedee")
+
 
 
 class GameServer(Server):
@@ -48,6 +46,7 @@ class GameServer(Server):
     def __init__(self, *args, **kwargs):
         Server.__init__(self, *args, **kwargs)
         self.players = WeakKeyDictionary()
+        self._map = Map(70, 50)
         print('Server launched')
         logging.info("Server Launched")
 
@@ -59,8 +58,7 @@ class GameServer(Server):
         self.players[player] = True
         print("players", [p for p in self.players])
         time.sleep(2)
-        player.Send({"action": "connected", "message":""})
-        player.Send({"action": "recv_map", "message": _map.game_map})
+        player.Send({"action": "recv_map", "message": pickle.dumps(self._map.game_map)})
 
     def DelPlayer(self, player):
         print("Deleting Player" + str(player.addr))
@@ -70,7 +68,7 @@ class GameServer(Server):
         [p.Send({"action": action, "message": message}) for p in self.players]
 
     def Launch(self):
-        _map.generate_Dungeon(70, 50)
+        self._map.generate_Dungeon(70, 50)
         while True:
             self.Pump()
             time.sleep(0.0001)
--- server.py
+++ server.py
@@ -7,3 +7,16 @@ server = GameServer(localaddr=(socket.gethostbyname(socket.gethostname()), 32078
 print(socket.gethostbyname(socket.gethostname()))
 
 server.Launch()
+
+ready = False
+while not ready:
+    print("Ready?")
+    for p in server.players:
+        print(p.nickname)
+    c = input()
+    if c.upper() == "YES" or c.upper() == "Y":
+        ready = True
+    else:
+        ready = False
+
+server.SendToAll()
