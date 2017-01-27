816020dad1e7f180b687ea3f3d1f87c7c751d876
Tom Pinnock (Linux) <tompinncock@gmail.com>
2017-01-27 10:58:37 +0000

    changes

--- server.py
+++ server.py
@@ -2,21 +2,9 @@ from network.Server import GameServer
 from game.Player import Player
 import socket
 
-server = GameServer(localaddr=(socket.gethostbyname(socket.gethostname()), 32078))
+server = GameServer(localaddr=("0.0.0.0", 32078))
 
 print(socket.gethostbyname(socket.gethostname()))
 
 server.Launch()
 
-ready = False
-while not ready:
-    print("Ready?")
-    for p in server.players:
-        print(p.nickname)
-    c = input()
-    if c.upper() == "YES" or c.upper() == "Y":
-        ready = True
-    else:
-        ready = False
-
-server.SendToAll()
